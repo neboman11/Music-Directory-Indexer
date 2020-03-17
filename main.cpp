036f8322451be22d6cbeac232e867150837b1771
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2020-03-17 09:23:05 -0500

    Added showing new and missing data to stdout

--- main.cpp
+++ main.cpp
@@ -13,7 +13,9 @@ namespace fs = std::filesystem;
 enum CmdOptions {
 	DIRECTORY,
 	INPUT,
-	OUTPUT
+	OUTPUT,
+	SORT_BY_ARTIST,
+	SORT_BY_ALBUM
 };
 
 map<int, string> parseCmd(int argc, char** argv);
@@ -23,6 +25,9 @@ int main(int argc, char** argv)
 {
 	vector<string*> givenData;		// A vector containing the data held in the input file
 	map<int, string> givenOptions;	// A map of the command line options given by the user
+	vector<string*> foundData;		// A vector containing all the data found in the file tree
+	vector<string*> newData;		// A vector containing all the new data found in the file tree
+	vector<string*> missingData;	// A vector containing all the read in data that was not found in the file tree
 
 	givenOptions = parseCmd(argc, argv);
 	// TODO handle an empty map
@@ -31,17 +36,6 @@ int main(int argc, char** argv)
 	if (givenOptions[INPUT] != "")
 	{
 		givenData = readGivenData(givenOptions[INPUT]);
-	}
-
-	// The output file stream for writing the indexed data
-	ofstream outFile(givenOptions[OUTPUT]);
-
-	// If the output file could not be opened
-	if (!outFile)
-	{
-		// Let the user know
-		cerr << "Failed to open " << givenOptions[OUTPUT] << "." << endl;
-		return 1;
 	}
 
 	// Loop through every artist folder
@@ -56,8 +50,10 @@ int main(int argc, char** argv)
 				// Make sure that the current item is a folder
 				if (is_directory(album.path()))
 				{
-					// Write the artist and album as an entry in the output file
-					outFile << artist.path().filename() << ',' << album.path().filename() << endl;
+					string* temp1 = new string[2];
+					temp1[0] = artist.path().filename().string();
+					temp1[1] = album.path().filename().string();
+					foundData.push_back(temp1);
 
 					// If the user provided an input file
 					if (givenOptions[INPUT] != "")
@@ -79,8 +75,10 @@ int main(int argc, char** argv)
 						// If no match was found
 						if (!matchFound)
 						{
-							// Print the unmatched artist and album entry as it would appear in the output file
-							cout << artist.path().filename() << ',' << album.path().filename() << endl;
+							string* temp2 = new string[2];
+							temp2[0] = artist.path().filename().string();
+							temp2[1] = album.path().filename().string();
+							newData.push_back(temp2);
 						}
 					}
 				}
@@ -88,13 +86,98 @@ int main(int argc, char** argv)
 		}
 	}
 
+	// Sort the data first
+
+	// The output file stream for writing the indexed data
+	ofstream outFile(givenOptions[OUTPUT]);
+
+	// If the output file could not be opened
+	if (!outFile)
+	{
+		// Let the user know
+		cerr << "Failed to open " << givenOptions[OUTPUT] << "." << endl;
+		return 1;
+	}
+
+	for (string* s : foundData)
+	{
+		// Write the artist and album as an entry in the output file
+		outFile << "\"" << s[0] << "\"" << ',' << "\"" << s[1] << "\"" << endl;
+	}
+
 	// Close the output file
 	outFile.close();
+
+	// If the user provided an input file
+	if (givenOptions[INPUT] != "")
+	{
+		// Sort the data first
+
+		// Print the new data to stdout
+		// Header
+		cout << "=====================NEW-DATA=====================\n";
+
+		for (string* s : newData)
+		{
+			cout << "\"" << s[0] << "\"" << ',' << "\"" << s[1] << "\"" << endl;
+		}
+
+		cout << endl;
+
+		// Find missing data
+		// Loop through the given data
+		for (string* givenE : givenData)
+		{
+			bool givenFound = false;
+
+			for (string* foundE : foundData)
+			{
+				if (givenE[0] == foundE[0] && givenE[1] == foundE[1])
+				{
+					givenFound = true;
+				}
+			}
+
+			if (!givenFound)
+			{
+				missingData.push_back(givenE);
+			}
+		}
+
+		// Sort the data first
+
+		// Print the missing data to stdout
+		// Header
+		cout << "===================MISSING-DATA===================\n";
+
+		for (string* s : missingData)
+		{
+			cout << "\"" << s[0] << "\"" << ',' << "\"" << s[1] << "\"" << endl;
+		}
+
+		cout << endl;
+	}
 
 	// Clear the used memory
 	for (unsigned long i = 0; i < givenData.size(); i++)
 	{
 		delete[] givenData[i];
+	}
+
+	// Clear the used memory
+	for (unsigned long i = 0; i < foundData.size(); i++)
+	{
+		delete[] foundData[i];
+	}
+
+	// If the user provided an input file
+	if (givenOptions[INPUT] != "")
+	{
+		// Clear the used memory
+		for (unsigned long i = 0; i < newData.size(); i++)
+		{
+			delete[] newData[i];
+		}
 	}
 
 	// We're done
